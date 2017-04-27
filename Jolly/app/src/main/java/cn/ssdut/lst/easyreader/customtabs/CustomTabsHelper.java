83fd9929c94d152baa8c0cdbd70d147f4a1a54c4
lee <501648152@qq.com>
2017-04-27 21:17:14 +0800

    practice some widgets in Android-Support-Design package

--- Jolly/app/src/main/java/cn/ssdut/lst/easyreader/customtabs/CustomTabsHelper.java
+++ Jolly/app/src/main/java/cn/ssdut/lst/easyreader/customtabs/CustomTabsHelper.java
@@ -2,6 +2,13 @@ package cn.ssdut.lst.easyreader.customtabs;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.text.TextUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by lisongting on 2017/4/25.
@@ -24,6 +31,56 @@ public class CustomTabsHelper {
 
     public static void addKeepAliveExtra(Context context, Intent intent) {
         Intent keepAliveIntent = new Intent().setClassName(context.getPackageName(), KeepAliveService.class.getCanonicalName());
+        intent.putExtra(EXTRA_CUSTOM_TABS_KEEP_ALIVE, keepAliveIntent);
+    }
+
+    public static String getsPackageNameToUse(Context context) {
+        if (sPackageNameToUse != null) {
+            return sPackageNameToUse;
+        }
+        PackageManager pm = context.getPackageManager();
+
+        Intent activityIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.example.com"));
+        ResolveInfo defaultViewHandlerInfo = pm.resolveActivity(activityIntent, 0);
+        String defaultViewHandlerPackageName = null;
+        if (defaultViewHandlerInfo != null) {
+            defaultViewHandlerPackageName = defaultViewHandlerInfo.activityInfo.packageName;
+        }
+
+        //获取所有能够处理这个Intent的App
+        List<ResolveInfo> resolvedActivityList = pm.queryIntentActivities(activityIntent, 0);
+        List<String> packageSupportCustomTabs = new ArrayList<>();
+        for (ResolveInfo r : resolvedActivityList) {
+            Intent serviceIntent = new Intent(ACTION_CUSTOM_TABS_CONNECTION);
+            serviceIntent.setPackage(r.activityInfo.packageName);
+            if (pm.resolveService(serviceIntent, 0) != null) {
+                packageSupportCustomTabs.add(r.activityInfo.packageName);
+            }
+        }
+
+        if (packageSupportCustomTabs.isEmpty()) {
+            sPackageNameToUse = null;
+        }  else if (packageSupportCustomTabs.size() == 1) {
+            sPackageNameToUse = packageSupportCustomTabs.get(0);
+        } else if (!TextUtils.isEmpty(defaultViewHandlerPackageName)
+                && !hasSpecializedHandlerIntents(context, activityIntent)
+                && packageSupportCustomTabs.contains(defaultViewHandlerPackageName)) {
+            sPackageNameToUse = defaultViewHandlerPackageName;
+        } else if (packageSupportCustomTabs.contains(STABLE_PACKAGE)) {
+            sPackageNameToUse = STABLE_PACKAGE;
+        } else if (packageSupportCustomTabs.contains(BETA_PACKAGE)) {
+            sPackageNameToUse = BETA_PACKAGE;
+        } else if (packageSupportCustomTabs.contains(DEV_PACKAGE)) {
+            sPackageNameToUse = DEV_PACKAGE;
+        } else if (packageSupportCustomTabs.contains(LOCAL_PACKAGE)) {
+            sPackageNameToUse = LOCAL_PACKAGE;
+        }
+        return sPackageNameToUse;
+    }
+
+    private static boolean hasSpecializedHandlerIntents(Context context, Intent activityIntent) {
+        try{
+        }
     }
 
 }
---
+++ UIEffects/supportdesignwidget/src/main/java/cn/ssdut/lst/supportdesignwidget/MainActivity.java
@@ -0,0 +1,67 @@
+package cn.ssdut.lst.supportdesignwidget;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    NavigationView navigationView;
+    FloatingActionButton fab;
+    CoordinatorLayout coordinatorLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        navigationView = (NavigationView) findViewById(R.id.navi_view);
+        fab = (FloatingActionButton) findViewById(R.id.id_fab);
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.navi_gallery) {
+                    Toast.makeText(MainActivity.this, "selected gallery item", Toast.LENGTH_SHORT).show();
+                } else if (id == R.id.navi_contact) {
+                    Toast.makeText(MainActivity.this, "selected contact item", Toast.LENGTH_SHORT).show();
+                } else if (id == R.id.navi_slideshow) {
+                    Toast.makeText(MainActivity.this, "selected slideshow item", Toast.LENGTH_SHORT).show();
+                } else if (id == R.id.navi_share) {
+                    Toast.makeText(MainActivity.this, "selected share item", Toast.LENGTH_SHORT).show();
+                }
+                DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Clicked FloatingActionButton", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public void createSnackbar(View v) {
+        //snackbar是一个类似Toast一样可以短暂停留的提示组件。可以向右滑动让其消失
+        Snackbar.make(coordinatorLayout, "this is a SnackBar.", Snackbar.LENGTH_LONG)
+                .setAction("确定", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "点击了确定按钮", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .show();
+    }
+
+}
