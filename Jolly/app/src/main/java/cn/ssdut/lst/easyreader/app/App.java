970609be7ebe6b829d5dade0d0c11914e641adfb
Lee <501648152@qq.com>
2017-05-22 20:59:11 +0800

    codes

--- Jolly/app/src/main/java/cn/ssdut/lst/easyreader/app/App.java
+++ Jolly/app/src/main/java/cn/ssdut/lst/easyreader/app/App.java
@@ -1,6 +1,7 @@
 package cn.ssdut.lst.easyreader.app;
 
 import android.app.Application;
+import android.content.ContentProvider;
 import android.support.v7.app.AppCompatDelegate;
 
 /**
@@ -10,11 +11,15 @@ import android.support.v7.app.AppCompatDelegate;
 public class App extends Application {
 
     public void onCreate() {
+        super.onCreate();
         //0代表白天主题，1代表夜间主题
         if (getSharedPreferences("user_settings", MODE_PRIVATE).getInt("theme", 0) == 0) {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         }else{
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         }
+
+        ContentProvider k;
     }
+
 }
--- TTS/xunfeitts/src/main/java/com/example/xunfeitts/MainActivity.java
+++ TTS/xunfeitts/src/main/java/com/example/xunfeitts/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.xunfeitts;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
