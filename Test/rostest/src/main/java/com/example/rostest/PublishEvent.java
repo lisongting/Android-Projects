4442f3599f426dd3b0ad49f4097d0b4962ee92a8
Lee <501648152@qq.com>
2017-06-03 21:39:24 +0800

    test ros in project

---
+++ Test/rostest/src/main/java/com/example/rostest/PublishEvent.java
@@ -0,0 +1,31 @@
+package com.example.rostest;
+
+
+import com.example.rostest.ros.rosbridge.operation.Operation;
+
+/**EventBus event entity,describe ros server response info
+ * Created by xxhong on 16-11-22.
+ */
+
+public class PublishEvent {
+    public String msg;
+    public String id;
+    public String name;
+    public String op;
+
+
+    public PublishEvent(Operation operation, String name, String content) {
+        if(operation != null) {
+            id = operation.id;
+            op = operation.op;
+        }
+        this.name = name;
+        msg = content;
+    }
+
+    // FIXME: maybe toLogcat() is more accurate?
+    public String toString() {
+        return "{msg: '" + msg + "', id: '" + id + "', name: '"
+                + name + "', op: '" + op + "'}";
+    }
+}
