f0774323db6c41967e2ae6a6dac4c771df890285
Lee <501648152@qq.com>
2018-05-06 22:38:05 +0800

    practice

---
+++ KotlinProject/app/src/main/java/com/lst/kotlinproject/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.lst.kotlinproject
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textview.text = "Kotlin"
+
+    }
+}
+
+
